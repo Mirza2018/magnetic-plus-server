51a979055aa75323b981d5790418d05cb522a5ca
unknown <mirza.eee.4th@gmail.com>
2024-05-30 10:21:06 +0600

    get Products

--- index.js
+++ index.js
@@ -15,24 +15,35 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@magneti
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+
+        const productCollection = client.db('MERN').collection('products')
+
+        app.get('/products', async (req, res) => {
+            const data = productCollection.find();
+            const result = await data.toArray()
+            res.send(result)
+        })
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -49,5 +60,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
 
     console.log(`Magnetic-plus server is running on port ${port}`);
-    
+
 })
